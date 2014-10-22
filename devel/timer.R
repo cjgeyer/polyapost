1a31022674ed54279746c0a9b8e02fc5cc289e79
Charles Geyer <charlie@stat.umn.edu>
2014-10-22 07:13:15 -0500

    timer.R
    This is ugly.  Even without finding the vertices, still does
    not scale well.  Probably the call to redundant.

---
+++ devel/timer.R
@@ -0,0 +1,24 @@
+
+ library(polyapost, lib.loc = "../package/polyapost.Rcheck")
+
+ d <- 200
+ x <- 1:d
+ # equality constraints
+ #     mean equal to (d + 1) / 2, that is, sum(x * p) = (d + 1) / 2
+ # inequality constraints
+ #     median less than or equal to (d + 1) / 2, that is,
+ #         sum(p[x <= (d + 1) / 2]) <= 1 / 2
+ a2 <- rbind(x)
+ b2 <- (d + 1) / 2
+ a1 <- rbind(as.numeric(x <= (d + 1) / 2))
+ b1 <- 1 / 2
+ # simulate prior, which Dirichlet(alpha)
+ # posterior would be another Dirichlet with n + alpha - 1,
+ #    where n is count of IID data for each value
+ alpha <- rep(2.3, d)
+ # Rprof()
+ out <- hitrun(alpha, nbatch = 30, blen = 250,
+     a1 = a1, b1 = b1, a2 = a2, b2 = b2)
+ # Rprof(NULL)
+ out$time
+
